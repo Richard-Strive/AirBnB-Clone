eb212e2f1ebea2bdc54174334c95322f9050c28d
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-01 12:58:35 +0100

    Before lunch...
    I'm gonna be right back

--- my-airbnb-clone/src/App.js
+++ my-airbnb-clone/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>I'm app.js</h1>
+      {/* Pseudo coddiamo lol. inziamo a dare un "ombra al nostro codice" tutta la bella compagnia sottostante e' contenuta nella home */}
+
+      {/*Home*/}
+      {/* Header */}
+      {/* Banner */}
+      {/*Search*/}
+      {/* Cards */}
+      {/* Footer */}
+      <Home />
     </div>
   );
 }
