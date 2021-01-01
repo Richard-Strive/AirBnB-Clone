b1eaa312ecffad8e25234021971f631c5de4588a
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-01 23:27:02 +0100

    Work in progress

--- my-airbnb-clone/src/components/subcomponents/Banner.jsx
+++ my-airbnb-clone/src/components/subcomponents/Banner.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 import "../subStylings/Banner.css";
+
 function Banner() {
   return (
     <div className="banner">
+      <div className="banner_search">
+        <Button className="banner_searchButton" variant="outlined">
+          Search Date
+        </Button>
+      </div>
       <div className="banner_info">
         <h1>Get out and stretch your imagination</h1>
         <h5>
           Plan a differentknd of getaway to uncover the hidden gems near you
         </h5>
+        <Button variant="outlined">Explore Nearby</Button>
       </div>
     </div>
   );
