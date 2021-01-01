3365a0d15b0c65a87c11fcf6c52c896a2e8c2faf
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-02 00:28:10 +0100

    okok

--- my-airbnb-clone/src/components/subcomponents/Banner.jsx
+++ my-airbnb-clone/src/components/subcomponents/Banner.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@material-ui/core";
+import Search from "./Search";
 import "../subStylings/Banner.css";
 
 function Banner() {
+  const [showDate, setDate] = useState(true);
+  const handleDate = () => {
+    if (showDate) {
+      setDate(false);
+    } else {
+      setDate(true);
+    }
+  };
   return (
     <div className="banner">
       <div className="banner_search">
-        <Button className="banner_searchButton" variant="outlined">
-          Search Date
+        {!showDate && <Search />}
+        <Button
+          className="banner_searchButton"
+          variant="outlined"
+          onClick={handleDate}
+        >
+          {(showDate && "Search Date") || "Hide Date"}
         </Button>
       </div>
       <div className="banner_info">
---
+++ my-airbnb-clone/src/components/subcomponents/Search.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div className="searchnpm">
+      <h1>I'm the fucking component</h1>
+    </div>
+  );
+}
+
+export default Search;
