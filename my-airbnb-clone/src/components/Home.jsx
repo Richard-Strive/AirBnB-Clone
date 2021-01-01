2d8da97f9b099452f88e5924337a84807b52daf5
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-01 18:50:02 +0100

    Had to find a good IMAGE.

--- my-airbnb-clone/src/components/Home.jsx
+++ my-airbnb-clone/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./componentsStyling/Home.css";
+import Banner from "./subcomponents/Banner";
 
 function Home() {
   return (
     <div className="home">
       <h1>I'm the home component</h1>
+      <Banner />
     </div>
   );
 }
---
+++ my-airbnb-clone/src/components/subcomponents/Banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../subStylings/Banner.css";
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="banner_info">
+        <h1>Get out and stretch your imagination</h1>
+        <h5>
+          Plan a differentknd of getaway to uncover the hidden gems near you
+        </h5>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
