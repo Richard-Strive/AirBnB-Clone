8f1161d99f5d3053efa1b9863cc0887c437d35cf
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-02 14:00:35 +0100

    Had to fix the footer later

--- my-airbnb-clone/src/App.js
+++ my-airbnb-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       {/* Footer */}
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ my-airbnb-clone/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./componentsStyling/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        © 2021 AirBnB. Right reserved to STRIVE SCHOOL. Just joking it's a CLONE
+        folks :)
+      </p>
+      <p>Privacy - Terms - Happy New Year</p>
+    </div>
+  );
+}
+
+export default Footer;
