749af5b4d824cda378e3eecc32daf50f2e1b78a5
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-02 13:22:14 +0100

    Home finished

--- my-airbnb-clone/src/components/Home.jsx
+++ my-airbnb-clone/src/components/Home.jsx
@@ -1,12 +1,52 @@
 import React from "react";
 import "./componentsStyling/Home.css";
+import Card from "./subcomponents/Card";
 import Banner from "./subcomponents/Banner";
 
 function Home() {
   return (
     <div className="home">
-      <h1>I'm the home component</h1>
       <Banner />
+      <div className="home_section">
+        <Card
+          src="https://image.shutterstock.com/image-photo/view-blue-domes-bell-towers-600w-1509942527.jpg"
+          title="La Grecia"
+          description="Tuffatevi nello splendido paese di miti e leggende. Indiementicabili emozioni vi attendo."
+          price={1500}
+        />
+        <Card
+          src="https://image.shutterstock.com/image-photo/view-blue-domes-bell-towers-600w-1509942527.jpg"
+          title="La Grecia"
+          description="Tuffatevi nello splendido paese di miti e leggende. Indiementicabili emozioni vi attendo."
+          price={1500}
+        />
+        <Card
+          src="https://image.shutterstock.com/image-photo/view-blue-domes-bell-towers-600w-1509942527.jpg"
+          title="La Grecia"
+          description="Tuffatevi nello splendido paese di miti e leggende. Indiementicabili emozioni vi attendo."
+          price={1500}
+        />
+      </div>
+      <div className="home_section">
+        <Card
+          src="https://image.shutterstock.com/image-photo/view-blue-domes-bell-towers-600w-1509942527.jpg"
+          title="La Grecia"
+          description="Tuffatevi nello splendido paese di miti e leggende. Indiementicabili emozioni vi attendo."
+          price={1500}
+        />
+        <Card
+          src="https://image.shutterstock.com/image-photo/view-blue-domes-bell-towers-600w-1509942527.jpg"
+          title="La Grecia"
+          description="Tuffatevi nello splendido paese di miti e leggende. Indiementicabili emozioni vi attendo."
+          price={1500}
+        />
+        <Card
+          src="https://image.shutterstock.com/image-photo/view-blue-domes-bell-towers-600w-1509942527.jpg"
+          title="La Grecia"
+          description="Tuffatevi nello splendido paese di miti e leggende. Indiementicabili emozioni vi attendo."
+          price={1500}
+        />
+      </div>
     </div>
   );
 }
